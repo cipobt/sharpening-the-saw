37c1eea6085f034214a5f23e3f1be86c9d1f73d5
aryavachin <aryavachin@gmail.com>
2024-10-07 18:42:41 +0100

    Added Fibonacci sequence function

--- script.py
+++ script.py
@@ -4,6 +4,17 @@ def factorial(n):
     else:
         return n * factorial(n-1)
 
+def fibonacci(n):
+    fib_seq = [0, 1]
+    for i in range(2, n):
+        fib_seq.append(fib_seq[-1] + fib_seq[-2])
+    return fib_seq[:n]
+
 number = int(input("Enter a number: "))
 print(f"The factorial of {number} is {factorial(number)}")
 
+number = int(input("Enter the length of the Fibonacci sequence: "))
+if number < 0:
+    print("Please enter a positive number")
+else:
+    print(f"Fibonacci sequence up to {number}: {fibonacci(number)}")
