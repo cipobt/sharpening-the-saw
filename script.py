66cf15667e2950c67dc4ec530a634e2b14eccad1
aryavachin <aryavachin@gmail.com>
2024-10-07 20:11:40 +0100

    Added Prime Number Checker function

--- script.py
+++ script.py
@@ -4,17 +4,38 @@ def factorial(n):
     else:
         return n * factorial(n-1)
 
+
 def fibonacci(n):
     fib_seq = [0, 1]
     for i in range(2, n):
         fib_seq.append(fib_seq[-1] + fib_seq[-2])
     return fib_seq[:n]
 
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+
 number = int(input("Enter a number: "))
 print(f"The factorial of {number} is {factorial(number)}")
+
 
 number = int(input("Enter the length of the Fibonacci sequence: "))
 if number < 0:
     print("Please enter a positive number")
 else:
     print(f"Fibonacci sequence up to {number}: {fibonacci(number)}")
+
+
+number = int(input("Enter a number: "))
+if is_prime(number):
+    print(f"{number} is a prime number")
+else:
+    print(f"{number} is not a prime number")
+
