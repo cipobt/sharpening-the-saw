4ead624a8ec0bfdfe21ec3ec48d8cca85fa3257a
aryavachin <aryavachin@gmail.com>
2024-10-11 15:19:48 +0100

    Function to count number of vowels in a given string added

--- palindrome.py
+++ palindrome.py
@@ -2,8 +2,18 @@ def is_palindrome(word):
     cleaned_word = ''.join(e.lower() for e in word if e.isalnum())
     return cleaned_word == cleaned_word[::-1]
 
+def count_vowels(s):
+    vowels = 'aeiou'
+    return sum(1 for char in s.lower() if char in vowels)
+
+
+
 word = input("Enter a word or phrase: ")
 if is_palindrome(word):
     print(f"'{word}' is a palindrome!")
 else:
     print(f"'{word}' is not a palindrome.")
+
+
+user_string = input("Enter a string: ")
+print(f"Number of vowels: {count_vowels(user_string)}")
