a8f5d739cca4d899f1579e2e6ee653e810f4d5f9
aryavachin <aryavachin@gmail.com>
2024-10-17 19:02:30 +0100

    Function to Convert Seconds to Hours, Minutes, and Seconds added

--- timer.py
+++ timer.py
@@ -9,8 +9,26 @@ def countdown(seconds):
         seconds -= 1
     print("Time's up!")
 
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    remaining_seconds = seconds % 60
+    return hours, minutes, remaining_seconds
+
+
+
+
 try:
     seconds = int(input("Enter time in seconds: "))
     countdown(seconds)
+except ValueError:
+    print("Please enter a valid number of seconds.")
+
+
+
+try:
+    total_seconds = int(input("Enter time in seconds: "))
+    h, m, s = convert_seconds(total_seconds)
+    print(f"{total_seconds} seconds is equal to {h} hour(s), {m} minute(s), and {s} second(s).")
 except ValueError:
     print("Please enter a valid number of seconds.")
