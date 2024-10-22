57f0b3ab5f8998cb3ad9dde74d4ee2594818ce02
aryavachin <aryavachin@gmail.com>
2024-10-22 20:51:46 +0100

    Simple function to return length of string entered by user added

--- words_play.py
+++ words_play.py
@@ -1,5 +1,15 @@
 def capitalize_words(sentence):
     return sentence.title()
 
+def string_length(s):
+    return len(s)
+
+
+
 sentence = input("Enter a sentence: ")
 print(f"Capitalized sentence: {capitalize_words(sentence)}")
+
+
+user_input = input("Enter a string: ")
+print(f"The length of the string is: {string_length(user_input)}")
+
