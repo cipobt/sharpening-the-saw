caa1c35687f552dd199a5995825d98bc301e8b8e
aryavachin <aryavachin@gmail.com>
2024-11-11 20:45:14 +0000

    Added check_win method to check if current player has won

--- tic-tac-toe-ai/game_engine.py
+++ tic-tac-toe-ai/game_engine.py
@@ -17,3 +17,14 @@ class GameEngine:
 
     def switch_player(self):
         self.current_player = "O" if self.current_player == "X" else "X"
+
+    def check_win(self):
+        board = self.board
+        for i in range(3):
+            if all([spot == self.current_player for spot in board[i]]) or \
+            all([board[j][i] == self.current_player for j in range(3)]):
+                return True
+        if board[0][0] == board[1][1] == board[2][2] == self.current_player or \
+        board[0][2] == board[1][1] == board[2][0] == self.current_player:
+            return True
+        return False
